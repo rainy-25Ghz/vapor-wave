cfdd1a72a7e27c96f4fe590fccb84b6541b44fbd
dd <1748420480@qq.com>
2022-06-05 23:39:07 +0800

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,51 @@
-
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectionShader.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader.js";
 
-const textureUrl=new URL("./grid.png",import.meta.url);
-
+const TEXTURE_PATH = new URL("./grid.png", import.meta.url).href;
+const DISPLACEMENT_PATH = new URL("./displacement.png", import.meta.url).href;
+const METALNESS_PATH = new URL("./metalness.png", import.meta.url).href;
 
 // Textures
 const textureLoader = new THREE.TextureLoader();
-const gridTexture = textureLoader.load(textureUrl.href);
+const gridTexture = textureLoader.load(TEXTURE_PATH);
+const terrainTexture = textureLoader.load(DISPLACEMENT_PATH);
+const metalnessTexture = textureLoader.load(METALNESS_PATH);
 
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
 // Scene
 const scene = new THREE.Scene();
 
+// Fog
+const fog = new THREE.Fog("#000000", 1, 2.5);
+scene.fog = fog;
+
 // Objects
 const geometry = new THREE.PlaneGeometry(1, 2, 24, 24);
 const material = new THREE.MeshStandardMaterial({
-    // Add the texture to the material
     map: gridTexture,
+    displacementMap: terrainTexture,
+    displacementScale: 0.4,
+    /**
+     * Add a metalnessMap to our material that will tell the renderer
+     * where the "rough" parts of our terrains are
+     */
+    metalnessMap: metalnessTexture,
+    /**
+     * Make the terrain very very metallic so it will reflect the light
+     * and not diffuse it: it will stay black
+     */
+    metalness: 0.96,
+    /**
+     * Make the terrain a bit rough so the rough parts will diffuse the light
+     * well
+     */
+    roughness: 0.5,
 });
 
 const plane = new THREE.Mesh(geometry, material);
@@ -26,29 +53,55 @@ plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = 0.0;
 plane.position.z = 0.15;
 
-scene.add(plane);
+const plane2 = new THREE.Mesh(geometry, material);
+plane2.rotation.x = -Math.PI * 0.5;
+plane2.position.y = 0.0;
+plane2.position.z = -1.85; // 0.15 - 2 (the length of the first plane)
 
-//lights
-const ambientLight = new THREE.AmbientLight(0xff50ff, 10);
+scene.add(plane);
+scene.add(plane2);
+
+// Light
+// Ambient Light
+const ambientLight = new THREE.AmbientLight("#2222ff", 10);
 scene.add(ambientLight);
+
+// Right Spotlight aiming to the left
+const spotlight = new THREE.SpotLight("#ff33ff", 20, 25, Math.PI * 0.1, 0.25);
+spotlight.position.set(0.5, 0.75, 2.2);
+// Target the spotlight to a specific point to the left of the scene
+spotlight.target.position.x = -0.25;
+spotlight.target.position.y = 0.25;
+spotlight.target.position.z = 0.25;
+scene.add(spotlight);
+scene.add(spotlight.target);
+
+// Left Spotlight aiming to the right
+const spotlight2 = new THREE.SpotLight("#ff33ff", 20, 25, Math.PI * 0.1, 0.25);
+spotlight2.position.set(-0.5, 0.75, 2.2);
+// Target the spotlight to a specific point to the right side of the scene
+spotlight2.target.position.x = 0.25;
+spotlight2.target.position.y = 0.25;
+spotlight2.target.position.z = 0.25;
+scene.add(spotlight2);
+scene.add(spotlight2.target);
 
 // Sizes
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.01,
-  20
+    75,
+    sizes.width / sizes.height,
+    0.01,
+    20
 );
 camera.position.x = 0;
 camera.position.y = 0.06;
 camera.position.z = 1.1;
-
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
@@ -56,10 +109,26 @@ controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Post Processing
+const effectComposer = new EffectComposer(renderer);
+effectComposer.setSize(sizes.width, sizes.height);
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const renderPass = new RenderPass(scene, camera);
+effectComposer.addPass(renderPass);
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader);
+rgbShiftPass.uniforms["amount"].value = 0.0015;
+
+effectComposer.addPass(rgbShiftPass);
+
+const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
+effectComposer.addPass(gammaCorrectionPass);
 
 // Event listener to handle screen resize
 window.addEventListener("resize", () => {
@@ -74,15 +143,26 @@ window.addEventListener("resize", () => {
     // Update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    // Update effect composer
+    effectComposer.setSize(sizes.width, sizes.height);
+    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
+
+const clock = new THREE.Clock();
 
 // Animate
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
     // Update controls
     controls.update();
 
-    // Update the rendered scene
-    renderer.render(scene, camera);
+    plane.position.z = (elapsedTime * 0.15) % 2;
+    plane2.position.z = ((elapsedTime * 0.15) % 2) - 2;
+
+    // Render
+    // renderer.render(scene, camera);
+    effectComposer.render();
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick);
